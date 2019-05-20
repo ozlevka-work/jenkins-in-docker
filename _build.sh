2f3fddbf5b9eca8eba5f3790b732a10a723f71ae
ozlevka-ericom <lev.ozeryansky@ericom.com>
2019-05-20 17:23:09 +0300

    Save changes in ansible branch

--- _build.sh
+++ _build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 docker build -t ozlevka/jenkins-ansible:latest .
-TAG=`date +"%y%m%d-%H.%M"`
-docker tag ozlevka/jenkins-ansible:latest "ozlevka/jenkins-ansible:$TAG"
-#docker push "ozlevka/jenkins:$TAG"
+
+if [[ "$1" = "upload" ]]; then
+    TAG=`date +"%y%m%d-%H.%M"`
+    docker tag ozlevka/jenkins-ansible:latest "ozlevka/jenkins-ansible:$TAG"
+    docker push "ozlevka/jenkins:$TAG"
+fi
