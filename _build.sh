b9f7cbe525166ed3c9af7e364a78ce24e3a4cfb5
Lev Ozeryansky <lev.ozeryansky@ericom.com>
2019-12-29 10:08:27 +0200

    Fix build script to the same container repo

--- _build.sh
+++ _build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker build -t ozlevka/jenkins-ansible:latest .
+docker build -t ozlevka/jenkins:latest .
 
 if [[ "$1" = "upload" ]]; then
     TAG=`date +"%y%m%d-%H.%M"`
-    docker tag ozlevka/jenkins-ansible:latest "ozlevka/jenkins-ansible:$TAG"
+    docker tag ozlevka/jenkins:latest "ozlevka/jenkins:$TAG"
     docker push "ozlevka/jenkins:$TAG"
 fi
